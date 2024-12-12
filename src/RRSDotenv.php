44cb1f877f995e7c79360c5afb80fd16a15d8b33
LLouzada <loumad.soft@gmail.com>
2024-12-12 16:37:39 -0300

    default fle (.env)

--- src/RRSDotenv.php
+++ src/RRSDotenv.php
@@ -6,19 +6,21 @@ class RRSDotenv
 {
     public static function load($filePath)
     {
-        if (!file_exists($filePath)) {
+        // File validation
+        $dotenvPath = $filePath . '/.env';
+        if (!file_exists($dotenvPath)) {
             return;
         }
 
-        $lines = file($filePath, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+        $lines = file($dotenvPath, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
 
         foreach ($lines as $line) {
-            // Ignorar comentários
+            // Ignore comments
             if (strpos(trim($line), '#') === 0) {
                 continue;
             }
 
-            // Dividir chave e valor
+            // Split the line into key and value
             $parts = explode('=', $line, 2);
             if (count($parts) !== 2) {
                 continue;
@@ -27,14 +29,14 @@ class RRSDotenv
             $key = trim($parts[0]);
             $value = trim($parts[1]);
 
-            // Remover aspas ao redor do valor
+            // Remove surrounding quotes
             if (preg_match('/^"(.*)"$/', $value, $matches)) {
                 $value = $matches[1];
             } elseif (preg_match("/^'(.*)'$/", $value, $matches)) {
                 $value = $matches[1];
             }
 
-            // Definir a variável de ambiente
+            // Set the environment variables
             if (!array_key_exists($key, $_ENV) && !array_key_exists($key, $_SERVER)) {
                 putenv("$key=$value");
                 $_ENV[$key] = $value;
